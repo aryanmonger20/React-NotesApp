397fbacd3e6409ddfcd68b36078b62ba8aff2a1b
Aryan Gupta <56786156+aryanmonger20@users.noreply.github.com>
2020-11-19 02:42:46 +0530

    Add files via upload

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    const { toggleNote, showNote } = this.props;
+    
+    return (
+      <div className="nav-container">
+        <div className="nav-logo">Notes</div>
+        <div className="nav-button" onClick={() => toggleNote()} >
+          { showNote ? 'Cancel' :  '+ Note' }
+        </div> 
+      </div>
+    );
+  }
+}
+
+export default Nav;
---
+++ src/components/NoteCard.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class NoteCard extends Component {
+  renderTags(note) {
+    return note.tags.map((tag, index) => 
+      <span className="note-card-tag" key={index}>
+        {tag.name}
+      </span>
+    );
+  }
+
+  render() {
+    const { note, getNote, deleteNote } = this.props;
+    return(
+      <div className="note-card-container">
+        <div className="note-card-title">
+          {note.title}
+        </div>
+        <div className="note-card-content">
+          {note.content}
+        </div>
+        <div className="note-card-tags">
+          {this.renderTags(note)}
+        </div>
+        <span className="note-card-delete" onClick={() => deleteNote(note.id)}>
+          <i className="material-icons">Delete</i>
+        </span>
+        <span className="note-card-edit" onClick={() => getNote(note.id)}>
+          <i className="">Edit</i>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default NoteCard;
